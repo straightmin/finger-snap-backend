ee10c594c7258f1c89f0d359ff2ce4074f36ecd6
straightmin <straightmin@gmail.com>
2025-07-05 02:53:12 +0900

    feat(auth): implement login API with JWT and validation

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -4,6 +4,7 @@ import bcrypt from "bcrypt";
 import prisma from "../lib/prisma";
 import { getMessage } from "../utils/messageMapper";
 import { RequestHandler } from "express";
+import { generateToken } from "../utils/generateToken";
 
 const healthCheck = {
     status: "ok",
@@ -16,6 +17,7 @@ const myInfo = {
     email: "straightmin@gmail.com",
 };
 
+// register api
 export const register: RequestHandler = async (req: Request, res: Response) => {
     const { username, email, password } = req.body;
     const lang = req.headers["accept-language"] === "en" ? "en" : "ko";
@@ -67,8 +69,59 @@ export const register: RequestHandler = async (req: Request, res: Response) => {
     }
 };
 
-export const login = (req: Request, res: Response) => {
-    res.send("login");
+// login api
+export const login: RequestHandler = async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+    const lang = req.headers["accept-language"] === "en" ? "en" : "ko";
+
+    try {
+        // 필수값 확인
+        if (!email || !password) {
+            res.status(400).json({
+                message: getMessage("MISSING_REQUIRED_FIELDS", lang),
+            });
+            return;
+        }
+
+        // 사용자 조회
+        const user = await prisma.user.findUnique({
+            where: { email },
+        });
+
+        if (!user) {
+            res.status(401).json({
+                message: getMessage("INVALID_EMAIL_OR_PASSWORD", lang),
+            });
+            return;
+        }
+
+        // 비밀번호 확인
+        const isMatch = await bcrypt.compare(password, user.passwordHash);
+
+        if (!isMatch) {
+            res.status(401).json({
+                message: getMessage("INVALID_EMAIL_OR_PASSWORD", lang),
+            });
+            return;
+        }
+
+        // JWT 토큰 생성
+        const token = generateToken({
+            userid: user.id,
+            email: user.email,
+            username: user.username,
+        });
+
+        res.status(200).json({
+            message: getMessage("SUCCESS.LOGIN", lang),
+            token,
+        });
+    } catch (err) {
+        console.error("Error during login:", err);
+        res.status(500).json({
+            errorCode: "INTERNAL_SERVER_ERROR",
+        });
+    }
 };
 export const me = (req: Request, res: Response) => {
     res.send(myInfo);
